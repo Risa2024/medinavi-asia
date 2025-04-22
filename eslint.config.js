518ecbf348d8ab9a6f5c27d8d5f8afe403b906fe
Risa Goto <omusubi5656dup@gmail.com>
2025-04-22 15:16:07 +0900

    fix:singlequote

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from "@eslint/js"
+import globals from "globals"
+import markdown from "@eslint/markdown"
+import css from "@eslint/css"
+import { defineConfig } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-  {
-    rules: {
-      "no-unused-vars": "error",// 意味：未使用の変数を定義するとエラー
-      "no-undef": "error",// 意味：未定義の変数を使用するとエラー
-      "eqeqeq": "error", // 意味：厳密等価演算子を使用する
-      "prefer-const": "error", // 意味：再代入されない変数はconstで宣言する
-      "no-extra-semi": "error", // 意味：余分なセミコロンを許可しない
+    { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+    { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+    {
+        files: ["**/*.md"],
+        plugins: { markdown },
+        language: "markdown/gfm",
+        extends: ["markdown/recommended"],
     },
-  },
-  pluginJs.configs.recommended,
-  eslintConfigPrettier,
-]);
+    { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    {
+        rules: {
+            noUnusedVars: "error", // 意味：未使用の変数を定義するとエラー
+            noUndef: "error", // 意味：未定義の変数を使用するとエラー
+            eqeqeq: "error", // 意味：厳密等価演算子を使用する
+            preferConst: "error", // 意味：再代入されない変数はconstで宣言する
+            noExtraSemi: "error", // 意味：余分なセミコロンを許可しない
+        },
+    },
+    eslintConfigPrettier,
+])
