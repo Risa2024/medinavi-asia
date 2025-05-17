ac238618823f9ace23418e572ba0a1269711f4ac
Risa Goto <omusubi5656dup@gmail.com>
2025-05-18 01:37:42 +0900

    fix:アプリケーション基盤強化とデバッグ環境の整備

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // HTTPSでアクセスされる場合にURLをHTTPSで生成
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // エラーログの強化
+        \DB::listen(function($query) {
+            Log::info('SQL実行', [
+                'query' => $query->sql,
+                'bindings' => $query->bindings,
+                'time' => $query->time
+            ]);
+        });
     }
 }
